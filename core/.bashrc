224cf4e7e9afed6f7269864c9cf5ef360de04c3e
Tom Lyttelton <tom.lyttelton@gmail.com>
2025-06-09 10:19:08 -0700

    Add i3 updates

--- core/.bashrc
+++ core/.bashrc
@@ -96,3 +96,7 @@ export PATH="$PATH:$(go env GOPATH)/bin"
 # Load local overrides if present (not tracked in Git)
 [ -f ~/.bashrc.local ] && source ~/.bashrc.local
 . "$HOME/.cargo/env"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- i3/.config/i3/screenlayout.sh
+++ i3/.config/i3/screenlayout.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 resolutions=(
-  "--output HDMI-1 --primary --mode 2560x1440 --pos 0x0 --rotate left --output HDMI-2 --mode 2560x1440 --pos 1440x410 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off"
-  "--output DP-3-1 --primary --mode 2560x1440 --pos 0x0 --rotate left --output DP-3-2 --mode 2560x1440 --pos 1440x410 --rotate normal"
+  "--output HDMI-1 --primary --mode 2560x1440 --pos 0x0 --rotate left --output HDMI-2 --mode 2560x1440 --pos 1440x410 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off",
+  "--output DP-1 --off --output DP-2 --off --output HDMI-1 --off --output DP-3 --off --output DP-3-8 --mode 2560x1440 --pos 1440x509 --rotate normal --output DP-3-1 --primary --mode 2560x1440 --pos 0x0 --rotate left"
 )
 
 for resolution in "${resolutions[@]}"; do
